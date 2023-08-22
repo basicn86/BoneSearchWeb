ef1b33c402490c8e52d91bb95cc5b9e894fa6c52
Nedim Basic <basicn86@gmail.com>
2023-08-22 03:56:51 -0400

    Fixed CSS

--- search.php
+++ search.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="search2.css" rel="stylesheet">
+    <link href="search.css" rel="stylesheet">
 
     <title>bonesear.ch</title>
 </head>
