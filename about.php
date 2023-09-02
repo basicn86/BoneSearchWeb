7ea08558a7af46500a911c82e09e54b6e76cdccf
Nedim Basic <basicn86@gmail.com>
2023-09-01 20:00:28 -0400

    Finished about page.

---
+++ about.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="about.css" rel="stylesheet">
+    <link href="topnav.css" rel="stylesheet">
+
+    <title>bonesear.ch &VerticalSeparator; About</title>
+
+    <?php include("include/gtag.include"); ?>
+</head>
+<body>
+    <header>
+        <?php include("include/topnav.html"); ?>
+    </header>
+
+    <main>
+        <h1>About bonesear<span class="tld">.ch</span></h1>
+        <p>Bonesearch is a next generation search engine, born from a vision to redefine how we search the internet.</p>
+        <p>Our ultimate aim is to deliver a truly personalized search engine experience for each and every user. In a digital landscape dominated by giants, we set out on a mission to offer something beyond the ordinary, something that empowers you, the user, in ways never seen before</p>
+        <p>Bonesearch is not just another search engine; it is YOUR search engine. Our mission is to revolutionize the way you explore the internet by putting the power of customization and personalization in your hands. We believe that your search experience should be tailored to your preferences, and with Bonesearch, you are the captain of your online adventure.</p>
+        <p>At Bonesearch, we understand the frustration of hitting paywalls when searching for valuable information. That's why we've made it effortless to take control. With a simple click, you can bid farewell to paywall websites and unlock a seamless browsing experience. Say goodbye to subscription based websites in your results.</p>
+        <p>Your search, your rules. Bonesearch empowers you to curate your results like never before. Our advanced content filtering feature allows you to refine your searches by excluding specific categories of results that don't align with your interests. Tailor your experience, and let the content that matters most to you shine through.</p>
+        <p><b>Bonesearch is currently work in progress</b>, despite this we're committed to continually enhancing your search experience. As we move forward, we have exciting new features in the development pipeline. However, please be aware that, like any other project in development, you may encounter ocassional bugs along the way. Bonesearch is currently in public beta. We appreciate your understanding and patience as we focus on refining and expanding our offerings. Your feedback is invaluable during this phase, helping us shape a search engine that truly meets your needs.</p>
+    </main>
+</body>
+</html>
